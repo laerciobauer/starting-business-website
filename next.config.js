ff2775c5b090fddaad37f1a456ec8f40f914c2c0
Bruna <53488109+brunaboeger@users.noreply.github.com>
2025-10-31 12:01:29 -0300

    fix: fix build errors

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
-const path = require('path');
+const path = require("path");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: process.env.NEXT_DIST_DIR || '.next',
-  output: process.env.NEXT_OUTPUT_MODE,
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname, '../'),
-  },
+  distDir: ".next",
   eslint: {
     ignoreDuringBuilds: true,
   },
